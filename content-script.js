ac8df88d6c4daffb62f91115ca3e8179366c4aee
wangchaeboll <wang.chaeboll@gmail.com>
2024-09-03 14:56:26 +0800

    having array of node list

--- content-script.js
+++ content-script.js
@@ -7,14 +7,17 @@ window.addEventListener('load', function() {
     console.log("Window loaded");
     const ini = document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items > :nth-child(1)")
     // Your code here
-    console.log(ini)
+    
+    const iya =document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items")
+    const berapa = iya.querySelectorAll("ytd-guide-entry-renderer").length
 
     ini.addEventListener("contextmenu", (e) => {
         e.preventDefault()
         console.log("hello")
     })
 
-    console.log("try")
+    console.log(ini)
+    console.log("total subscription awak adalah " + ( berapa - 1 ) + " orang." )
 });
 
 console.log("bolehke?")
