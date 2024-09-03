c48967e76a2db237620c8b480fc46c2c415c52c1
wangchaeboll <wang.chaeboll@gmail.com>
2024-09-03 15:38:38 +0800

    esok tambah functionality yang tak suruh reload , maybe when they change state ? sebab kalau macam ni susah oh if mungkin kene ade dua satu for dah tekan side panel , satu tak

--- content-script.js
+++ content-script.js
@@ -1,23 +1,33 @@
-window.addEventListener('DOMContentLoaded', () => {
+// window.addEventListener('DOMContentLoaded', () => {
     
-    console.log("boleh tak?")
-});
+//     console.log("boleh tak?")
+// });
 
 window.addEventListener('load', function() {
     console.log("Window loaded");
-    const ini = document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items > :nth-child(1)")
-    // Your code here
+    const iya = document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items");
+    const ini = document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items > :nth-child(1)");
     
-    const iya =document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items")
-    const berapa = iya.querySelectorAll("ytd-guide-entry-renderer").length
+    
+    
+    // ini.addEventListener("contextmenu", (e) => {
+    //     e.preventDefault()
+    //     console.log("hello")
+    // })
+    
+    if (iya){
+        console.log("process cantuman bermula")
+        const element = iya.querySelectorAll("ytd-guide-entry-renderer")
+        element.forEach(elem => {
+            elem.classList.add("tambah")
+        })
 
-    ini.addEventListener("contextmenu", (e) => {
-        e.preventDefault()
-        console.log("hello")
-    })
+        console.log("pencantuman skrip berjaya")
+        console.log(element)
+    }else{
+        console.log("gagal untuk dicantumkan skrip")
+    }
 
-    console.log(ini)
-    console.log("total subscription awak adalah " + ( berapa - 1 ) + " orang." )
 });
 
 console.log("bolehke?")
