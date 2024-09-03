202ba35cb9111ad2abd8f6303f356198ab492ab9
wangchaeboll <wang.chaeboll@gmail.com>
2024-09-03 12:42:39 +0800

    we did it guys i dont know what should i say hahah

--- content-script.js
+++ content-script.js
@@ -8,6 +8,13 @@ window.addEventListener('load', function() {
     const ini = document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items > :nth-child(1)")
     // Your code here
     console.log(ini)
+
+    ini.addEventListener("contextmenu", (e) => {
+        e.preventDefault()
+        console.log("hello")
+    })
+
+    console.log("try")
 });
 
 console.log("bolehke?")
