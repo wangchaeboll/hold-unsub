23d15277c0f2dba183dc52bf81f225043516f68e
wangchaeboll <wang.chaeboll@gmail.com>
2024-09-03 11:55:21 +0800

    prevent default example should use the context menu instead of button cause u can see clearly what happenning

--- content-script.js
+++ content-script.js
@@ -5,4 +5,11 @@ window.addEventListener('DOMContentLoaded', () => {
         console.log("hello")
     })
 });
-  
+
+// document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items > :nth-child(1)")
+
+
+// document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items > :nth-child(1)").addEventListener("contextmenu", function(e){
+//     e.preventDefault();
+//     console.log("hello")
+// })
