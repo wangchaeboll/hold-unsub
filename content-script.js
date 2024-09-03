9c520fd3a76844a46ad9e3ede547ae0046a67402
wangchaeboll <wang.chaeboll@gmail.com>
2024-09-03 12:38:35 +0800

    event for DOMcontentLoaded doesnt consoled out, while event load does so

--- content-script.js
+++ content-script.js
@@ -1,10 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector("#endpoint > tp-yt-paper-item > yt-formatted-string").style.color = "red"
-    document.querySelector("#endpoint > tp-yt-paper-item > yt-formatted-string").addEventListener("contextmenu", function(e){
-        e.preventDefault();
-        console.log("hello")
-    })
+    
+    console.log("boleh tak?")
 });
+
+window.addEventListener('load', function() {
+    console.log("Window loaded");
+    const ini = document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items > :nth-child(1)")
+    // Your code here
+    console.log(ini)
+});
+
+console.log("bolehke?")
+// document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items > :nth-child(1)").addEventListener("contextmenu", function(e){
+//         e.preventDefault();
+//         console.log("hello")
+//     })
+
+
 
 // document.querySelector("#content > #guide #contentContainer > #guide-wrapper > #guide-content > #guide-inner-content > #guide-renderer > #sections > ytd-guide-section-renderer:nth-child(2) > #items > :nth-child(1)")
 
